8a775c046d06b5da9841c25c6469d47b62cf3671
Nagarjuna Kumar <nagarjuna.kumar@duedil.com>
2016-07-05 12:54:25 +0100

    Added some documentation for functions

--- companycase/companycase.py
+++ companycase/companycase.py
@@ -14,6 +14,7 @@ class CompanyCase:
         return map(lambda x: ''.join(x), zip(*[input_list[i:] for i in range(n)]))
 
     def fetch_all_transitions(self, language, ngram_length):
+        """ Generate a dict of counts for transitions for all n-grams in the language word list """
         wordlist = os.path.join(os.path.dirname(__file__), "wordlists/{0}.txt".format(language))
         if not os.path.exists(wordlist):
             raise SystemError("Language '{0}' does not exist".format(language))
@@ -26,15 +27,16 @@ class CompanyCase:
                 all_grams += ngrams
         return dict(Counter(all_grams))
 
-
     def normalize_transitions(self, t):
         total = float(reduce(lambda x, y: x + y, t.values()))
         return dict([(x, y/total) for x, y in t.iteritems()])
 
     def force_case_for_words(self, l):
+        """ Add additional words to force case for"""
         self.force_case += l
 
     def score_word(self, word):
+        """ Returns the mean transition likelihood score for the word """
         ngrams = self.find_ngrams(word.lower(), self.ngram_length)
         if len(ngrams) < 1:
             return 0.0
@@ -45,18 +47,23 @@ class CompanyCase:
         fixed_name = []
         for word in company_name.split():
             fixed_word = None
+
+            # Check if the word is to be force cased
             for x in self.force_case:
                 if x.lower() == word.lower():
                     fixed_word = x
                     break
 
             if not fixed_word:
+                # Title case if dictionary word-like
+                # else, Upper case
                 score = self.score_word(word)
                 if score < threshold:
                     fixed_word = word.upper()
                 else:
                     fixed_word = word.title()
 
+                # Clean up any trailing 'S
                 if fixed_word.endswith("'S"):
                     fixed_word = fixed_word[:-1]+'s'
 
